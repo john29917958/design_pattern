283c06dd38db7acf7b4ba1a465d97b16638d4621
Tzu-Chao Wang <john29917958@gmail.com>
2022-08-31 08:15:10 +0800

    Add comment to input singleton

--- singleton/input/input.h
+++ singleton/input/input.h
@@ -4,10 +4,24 @@
 #include <cstdlib>
 #include <time.h>
 
+/**
+ * @brief Accessing pressed key.
+ */
 class input
 {
 public:
+    /**
+     * @brief Gets the singleton input instance.
+     * 
+     * @return input* Returns a input pointer.
+     */
     static input *instance();
+
+    /**
+     * @brief Gets the pressed input key.
+     * 
+     * @return char Returns the pressed input key.
+     */
     char get();
 
 private:
