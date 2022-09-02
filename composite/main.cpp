3426fda45aeed8c8c185e421fce5fa8081dcaf16
Tzu-Chao Wang <john29917958@gmail.com>
2022-09-02 08:10:05 +0800

    Add sample usage file file system classes

--- composite/main.cpp
+++ composite/main.cpp
@@ -1,4 +1,14 @@
+#include "file_system/directory.h"
+
 int main(int argc, char *argv[])
 {
+    compositefile::file_system* filesystem = new compositefile::file(".\\a-file");
+    filesystem->open();
+    filesystem->del();
+
+    filesystem = new compositefile::directory(".\\");
+    filesystem->open();
+    filesystem->del();
+
     return 0;
 }
