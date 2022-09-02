08ac7699944dfbe5a3b83973e1b8b7f8c78e7b69
Tzu-Chao Wang <john29917958@gmail.com>
2022-09-02 23:51:50 +0800

    Deletes copy constructor and assignment operator of singleton class
    Defines the copy constructor, assignment operator explicitly and deletes them from this singleton class, so it's not cloneable and not assignable. For example, this line of code is invalid:
    ```cpp
    input i = *input::instance(); // <= Compile error since copy constructor is deleted.
    ```

--- singleton/input/input.h
+++ singleton/input/input.h
@@ -11,15 +11,33 @@ class input
 {
 public:
     /**
-     * @brief Gets the singleton input instance.
+     * Defines the copy constructor explicitly
+     * and deletes it from this singleton class, so the input object is
+     * not cloneable. For example this line of code is invalid:
+     * @code {.cpp}
+     * input i = *input::instance(); // <= Compile error since copy constructor is deleted.
+     * @endcode
      * 
+     * @param other Reference to the object to be copied.     
+     */
+    input(const input &other) = delete;
+
+    /*
+     * Deletes the assignment operator since singleton object should not
+     * be assignable.
+     */
+    void operator=(const input &) = delete;
+
+    /**
+     * @brief Gets the singleton input instance.
+     *
      * @return input* Returns a input pointer.
      */
     static input *instance();
 
     /**
      * @brief Gets the pressed input key.
-     * 
+     *
      * @return char Returns the pressed input key.
      */
     char get();
