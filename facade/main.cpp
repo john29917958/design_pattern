ac37f8ebf3e940ee269bdcbf743b639e3cc612e6
Tzu-Chao Wang <john29917958@gmail.com>
2022-09-05 23:26:07 +0800

    Rename meal food

--- facade/main.cpp
+++ facade/main.cpp
@@ -8,14 +8,14 @@ int main(int argc, char *argv[])
     restaurant.load_chefs("res\\chefs");
     restaurant.load_recipes("res\\recipes");
 
-    std::shared_ptr<food> meal = restaurant.order("Hamburger");
-    if (meal == nullptr)
+    std::shared_ptr<food> food = restaurant.order("Hamburger");
+    if (food == nullptr)
     {
-        printf("Failed to order a meal: %s\n");
+        printf("Failed to order food: %s\n");
     }
     else
     {
-        printf("Got %s!\n", meal->get_name().c_str());
+        printf("Got %s!\n", food->get_name().c_str());
     }
 
     return 0;
